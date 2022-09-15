7441b8a4b1fd3467e324b85c6fba28bf53d1e913
Alvinashki <alvinikhsani14@gmail.com>
2022-09-15 20:02:06 +0700

    perbaikan order feature (auto migrate)

--- migration/migration.go
+++ migration/migration.go
@@ -2,6 +2,7 @@ package migration
 
 import (
 	cartModel "project/e-commerce/features/cart/data"
+	orderModel "project/e-commerce/features/order/data"
 	_mProducts "project/e-commerce/features/product/data"
 	userModel "project/e-commerce/features/user/data"
 
@@ -14,5 +15,6 @@ func InitMigrate(db *gorm.DB) {
 	// db.AutoMigrate(&Product.Product{})
 	db.AutoMigrate(&_mProducts.Product{})
 	db.AutoMigrate(&cartModel.Cart{})
+	db.AutoMigrate(&orderModel.Order{})
 
 }
